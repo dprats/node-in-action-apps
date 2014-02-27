522d449c1da0fe51482c622768c81dd332b67370
Diego Prats <diego.prats@gmail.com>
2014-02-27 14:46:35 -0800

    chapter 5 and redis

---
+++ 5/redis-app1/redis-server.js
@@ -0,0 +1,47 @@
+var redis = require('redis');
+var client = redis.createClient(6379, '127.0.0.1');
+
+client.on('error', function(err){
+	console.log('Error ' + err);
+});
+
+//he print function prints the results of an 
+//operation or an error if one occurs.
+client.set('color', 'red', redis.print);
+client.get('color', function(err, value){
+	if(err) throw err;
+	console.log('Got: ' + value);
+});
+
+//The hmset Redis command sets hash table 
+//elements, identified by a key, to a value.
+
+client.hmset('camping', {
+	'shelter': '2-person tent',
+	'cooking': 'campstove'
+}, redis.print);
+
+//Get “cooking” element’s value
+client.hget('camping', 'cooking', function(err, value){
+	if (err) throw err;
+	console.log('Will be cooing with: ' + value);
+});
+
+//Get hash table keys
+
+client.hkeys('camping', function(err, keys){
+	if(err) throw err;
+	keys.forEach(function(key){
+		console.log(' ' + key);
+	});
+});
+
+
+client.lpush('tasks', 'Paint the bike red', redis.print);
+client.lpush('tasks', 'Paint the bike green', redis.print);
+client.lrange('tasks', 0, -1, function(err, items){
+	if (err) throw err;
+	items.forEach(function(item, i){
+		console.log(' ' +  item);
+	});
+});
--- 5/timetrack/lib/timetrack.js
+++ 5/timetrack/lib/timetrack.js
@@ -66,19 +66,118 @@ exports.add = function(db, req, res){
 };
 
 exports.archive= function(db, req, res){
-
+	//parse HTTP data
+	exports.parseReceivedData(req, function(work){
+		db.query(
+			//SQL to archive the data
+			"UPDATE work SET archived=1 where id=?",
+			[work.id],
+			function(err){
+				if (err) throw err;
+				//show user a list of work records
+				exports.show(db, res);
+			}
+			);
+	});
 };
 
 //function to delete work
 
 exports.delete = function(db, req, res){
 
+	exports.parseReceivedData(req, function(work){
+		db.query(
+			//SQL to delete the work
+			"DELETE FROM work WHERE id =?",
+			[work.id],
+			function(err){
+				if(err) throw err;
+				//show user a list of work records
+				exports.show(db, res);
+			}
+			);
+	});
 };
 
-exports.show = function(db, req){
+exports.show = function(db, res, showArchived){
 
+//SQL to fetch work records
+	var query = "SELECT * FROM work " +
+							"WHERE archived=?" +
+							"ORDER BY date esc";
+
+	var archiveValue =(showArchived) ? 1 : 0;
+		db.query(
+			//SQL to show a particular work
+			query, 
+			[archiveValue],
+			function(err, rows){
+				if(err) throw err;
+				html = (showArchived)
+				? ''
+				: '<a href="/archived">Archived work</a><br/>';
+				//Format results as HTML table
+				html += exports.workHitListHtml(rows);
+				html += exports.workFormHtml(rows);
+				//Send HTML response to user
+				exports.sendHTML(res, html);
+			}
+		);
 };
 
-exports.showArchived =function(db, req){
-
+exports.showArchived =function(db, res){
+	//Show only archived work records
+	exports.show(db, res, true);
 };
+
+
+
+
+//rendering of work records to HTML.
+
+exports.workHitListHtml = function(rows){
+	var html = '<table>';
+	//Render each work record as HTML table row
+	for (var i in rows){
+		html += '<tr>'
+		html += '<td>' + rows[i].date + '</td>';
+		html += '<td>' + rows[i].hours + '</td>';
+		html += '<td>' + rows[i].description + '</td>';
+		//show archive button if work record isn’t already archived
+		if(!rows[i].archived){
+			html +='<td>' + exports.workArchiveForm(rows[i].id) +'</td>';
+		}
+		html += '<td>' + exports.workDeleteForm(rows[i].id) + '</td>';
+		html += '<tr>';
+	}
+	html +='</table>';
+	return html;
+};
+
+//render the HTML forms needed by the application.
+
+exports.workFormHtml = function(){
+	var html = '<form method="POST" action="/">' +
+							'<p>Date (YYYY-MM-DD):<br/><input name="date" type="text"></p>'+
+							'<p>Hours worked:<br/><input name="hours" type="text"></p>' +
+							'<p>Description:<br/>'+
+							'<textarea name="description"></textarea></p>' +
+							'<input type="submit" value= "Add" />' +
+							'</form>';
+	return html;
+};
+
+exports.workArchiveForm = function(id){
+	return exports.actionForm(id, '/archive', 'Archive');
+};
+
+exports.workDeleteForm = function(id){
+	return exports.actionForm(id, '/delete', 'Delete');
+};
+
+
+
+
+
+
+
